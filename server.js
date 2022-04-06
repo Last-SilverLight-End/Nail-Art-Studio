a2a11575f37f134cb824b2931e5209e8e7c44450
Last-SilverLight-End <68593892+Last-SilverLight-End@users.noreply.github.com>
2022-04-06 19:42:17 +0900

    fixing

--- server.js
+++ server.js
@@ -2,9 +2,28 @@ const express = require('express');
 const app = express();
 const multer = require("multer");
 const path = require("path");
-const PORT = 4000;
- 
- 
+const PORT = 5000;
+const router = express.Router();
+const upload = multer({ dest: 'uploads/' })
+
+app.use(express.static("public"));
+
+const storage = multer.diskStorage({
+   destination : "./uploads",
+    filename: function(req, file, cb)  {
+        cb(null, "imgfile" + Date.now() + path.extname(file.originalname));
+    },
+    }
+);
+
+ upload = multer({ storage: storage, limits: {fileSize : 1000000} }).single("profile_img");
+
+app.post("/profile", upload.array("img",12), function(req, res, next) {
+    res.send({
+      fileName: req.file.filename
+    });
+  });
+
 app.get('/data',(req,res)=>{
     const data = {
         lastname : "Lee",
@@ -16,3 +35,12 @@ app.get('/data',(req,res)=>{
 app.listen(PORT,()=>{
     console.log(`server running on PORT ${PORT}`);
 });
+
+
+
+
+app.post("/upload", upload.single("img"), function(req, res, next) {
+    res.send({
+      fileName: req.file.filename
+    });
+  });
