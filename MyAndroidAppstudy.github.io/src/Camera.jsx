670798dff011978a790941194d54f63105ee2661
Last-SilverLight-End <halfwing1998@gmail.com>
2022-05-01 19:25:44 +0900

    finish sending by taking pictures

--- MyAndroidAppstudy.github.io/src/Camera.jsx
+++ MyAndroidAppstudy.github.io/src/Camera.jsx
@@ -2,18 +2,65 @@ import React, { useState, useRef } from "react";
 import {Camera} from "react-camera-pro";
 import * as cocossd from "@tensorflow-models/coco-ssd";
 import "./App.css";
+import axios from 'axios';
+
+
 const Component = () => {
 
     const camera = useRef(null);
     const [numberOfCameras, setNumberOfCameras] = useState(0);
+    const [selectedFile, setSelectedFile] = useState(''); 
     const [image, setImage] = useState(null);
 
+    const todayTime = () => {
+      let now = new Date().toString();
+      return now;
+    }
+
+    const dataURLtoFile = (dataurl, fileName) => {
+ 
+      var arr = dataurl.split(','),
+          mime = arr[0].match(/:(.*?);/)[1],
+          bstr = atob(arr[1]), 
+          n = bstr.length, 
+          u8arr = new Uint8Array(n);
+          
+      while(n--){
+          u8arr[n] = bstr.charCodeAt(n);
+      }
+      
+      return new File([u8arr], fileName, {type:mime});
+  }
+
+    const submit = () =>{
+
+      const convertedFile = dataURLtoFile(image,todayTime()+".png");
+      const data = new FormData() 
+      data.append('file', convertedFile);
+      console.warn(convertedFile);
+
+      let url = "/uploader";
+
+      axios.post(url, data, {
+         // 주소와 formdata를 posting 한다
+      })
+      .then(res => { 
+        //상태 출력
+          console.warn(res);
+      })
+    }
 
   return (
     <div>
-      <Camera ref={camera} numberOfCamerasCallback={setNumberOfCameras} />
+      <Camera ref={camera} 
+      numberOfCamerasCallback={setNumberOfCameras}
+      aspectRatio={16 / 9}
+      facingMode = 'environment'
+       />
       
-      <img src={image} alt='이미지 미리보기' />
+      <img src={image} alt='이미지 미리보기'
+      
+       />
       <button
         onClick={() => {
             const photo = camera.current.takePhoto();
@@ -26,6 +73,8 @@ const Component = () => {
           camera.current.switchCamera();
         }}
       >카메라 전환 </button>
+      <button type ="submit"
+      onClick = {()=> submit()}> 업로드 하기</button>
     </div>
   );
 }
--- MyAndroidAppstudy.github.io/src/Users.jsx
+++ MyAndroidAppstudy.github.io/src/Users.jsx
@@ -1,18 +1,23 @@
 import React,{ useState,Component } from 'react';
 import "./App.css";
+import Camera from "react-camera-pro";
 import axios from 'axios';
 
 
 // 일단은 전송은 되나 나중에 사진 찍고 저장하여 파일 업로드는 구현 해야 한다.
 class Main extends React.Component{
+
+    
   constructor(){
+
       super();
       this.state = {
           selectedFile:'',
       }
+
       this.handleInputChange = this.handleInputChange.bind(this);
   }
-
+  
   handleInputChange(event) {
       this.setState({
           selectedFile: event.target.files[0],
@@ -37,28 +42,31 @@ class Main extends React.Component{
 
   render(){
       return(
-          <div>
-              <div className="row">
-                  <div className="col-md-6 offset-md-3">
-                      <br /><br />
+    <div>
+        <div >
+            <br /><br />
+                <h3>React File Upload Example - Tutsmake.com</h3>
+                <br />
+                <div>
+                    <div >
+                        <label >Select File :</label>
+                        <input type="file" 
+                        name="upload_file" 
+                        onChange={this.handleInputChange} />
+                    </div>
+                </div>
+                <div >
+                    <div>
+                        <button 
+                        type="submit"
+                        onClick={()=>this.submit()}>
+                            Save
+                            </button>
+                    </div>
+            </div>
+        </div>
 
-                          <h3 className="text-white">React File Upload Example - Tutsmake.com</h3>
-                          <br />
-                          <div className="form-row">
-                              <div className="form-group col-md-6">
-                                  <label className="text-white">Select File :</label>
-                                  <input type="file" className="form-control" name="upload_file" onChange={this.handleInputChange} />
-                              </div>
-                          </div>
-
-                          <div className="form-row">
-                              <div className="col-md-6">
-                                  <button type="submit" className="btn btn-dark" onClick={()=>this.submit()}>Save</button>
-                              </div>
-                          </div>
-                  </div>
-              </div>
-          </div>
+    </div>
       )  
   }
 }
