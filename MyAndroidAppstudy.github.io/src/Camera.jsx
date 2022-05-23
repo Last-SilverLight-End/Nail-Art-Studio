327fb07ff40fdcb618d440969eaf84fe1d7f426d
Last-SilverLight-End <68593892+Last-SilverLight-End@users.noreply.github.com>
2022-05-23 17:47:35 +0900

    fix camera ui

--- MyAndroidAppstudy.github.io/src/Camera.jsx
+++ MyAndroidAppstudy.github.io/src/Camera.jsx
@@ -7,6 +7,130 @@ import { Navigate, useNavigate } from "react-router";
 import * as tmImage from '@teachablemachine/image';
 //import { model } from "@tensorflow/tfjs";
 import Resizer from "react-image-file-resizer";
+
+import styled from 'styled-components';
+
+const Control = styled.div`
+  position: fixed;
+  display: flex;
+  right: 0;
+  width: 20%;
+  min-width: 130px;
+  min-height: 130px;
+  height: 100%;
+  background: rgba(0, 0, 0, 0.8);
+  z-index: 10;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding: 50px;
+  box-sizing: border-box;
+  flex-direction: column-reverse;
+  @media (max-aspect-ratio: 1/1) {
+    flex-direction: row;
+    bottom: 0;
+    width: 100%;
+    height: 20%;
+  }
+  @media (max-width: 400px) {
+    padding: 10px;
+  }
+`;
+
+const Wrapper = styled.div`
+  position: fixed;
+  width: 100%;
+  height: 100%;
+  z-index: 1;
+`;
+const FullScreenImagePreview = styled.div`
+  width: 100%;
+  height: 100%;
+  z-index: 100;
+  position: absolute;
+  background-color: black;
+  ${({ image }) => (image ? `background-image:  url(${image});` : '')}
+  background-size: contain;
+  background-repeat: no-repeat;
+  background-position: center;
+`;
+
+const ImagePreview = styled.div`
+  width: 100px;
+  height: 100px;
+  ${({ image }) => (image ? `background-image:  url(${image});` : '')}
+  background-size: contain;
+  background-repeat: no-repeat;
+  background-position: center;
+  @media (max-width: 400px) {
+    width: 50px;
+    height: 120px;
+  }
+`;
+
+const Button = styled.button`
+  outline: none;
+  color: white;
+  opacity: 1;
+  background: transparent;
+  background-color: transparent;
+  background-position-x: 0%;
+  background-position-y: 0%;
+  background-repeat: repeat;
+  background-image: none;
+  padding: 0;
+  text-shadow: 0px 0px 4px black;
+  background-position: center center;
+  background-repeat: no-repeat;
+  pointer-events: auto;
+  cursor: pointer;
+  z-index: 2;
+  filter: invert(100%);
+  border: none;
+  &:hover {
+    opacity: 0.7;
+  }
+`;
+
+const TakePhotoButton = styled(Button)`
+  background: url('https://img.icons8.com/ios/50/000000/compact-camera.png');
+  background-position: center;
+  background-size: 50px;
+  background-repeat: no-repeat;
+  width: 80px;
+  height: 80px;
+  border: solid 4px black;
+  border-radius: 50%;
+  &:hover {
+    background-color: rgba(0, 0, 0, 0.3);
+  }
+`;
+
+const ChangeFacingCameraButton = styled(Button)`
+  background: url(https://img.icons8.com/ios/50/000000/switch-camera.png);
+  background-position: center;
+  background-size: 40px;
+  background-repeat: no-repeat;
+  width: 40px;
+  height: 40px;
+  padding: 40px;
+  &:disabled {
+    opacity: 0;
+    cursor: default;
+    padding: 60px;
+  }
+  @media (max-width: 400px) {
+    padding: 40px 5px;
+    &:disabled {
+      padding: 40px 25px;
+    }
+  }
+`;
+
+
+
+
+
 const Component = () => {
 
   const camera = useRef(null);
@@ -15,7 +139,7 @@ const Component = () => {
   const [image, setImage] = useState(null);
   const navigate = useNavigate();
   const [showResult,setShowResult]=useState(false);
-  //const [photo,setPhoto] = useState('');
+  const [showImage, setShowImage] = useState(false);
   // const [isLoading,setIsLoading] = useState<Boolean>(false);
 
   // teachable machine 모텔 불러오기 코드
@@ -54,6 +178,8 @@ const Component = () => {
     maxPredictions = model.getTotalClasses();
 
   }
+
+  
 
 
 
@@ -130,33 +256,51 @@ const Component = () => {
   }
 
   return (
-    <div>
+    <Wrapper>
+      {showImage ? (
+        <FullScreenImagePreview
+          image={image}
+          onClick={() => {
+            setShowImage(!showImage);
+          }}
+          />
+      )
+      :(
       <Camera ref={camera}
         numberOfCamerasCallback={setNumberOfCameras}
-        aspectRatio={16 / 9}
+        aspectRatio="cover"
         facingMode='environment'
+        className = "image-size"
       />
-      <img id = "srcimg" className="image-size" src={image} alt='이미지 미리보기'
+      )}
 
-      />
-      <button
-        onClick={() => {
-          const photo = (camera.current.takePhoto());
-          setImage(photo);
-         /* predict();*/
-          
-        }}
-        
-      > 카메라 사진 찍기</button>
-      <button
-        hidden={numberOfCameras <= 1}
-        onClick={() => {
-          camera.current.switchCamera();
-        }}
-      >카메라 전환 </button>
-      <button type="submit"
-        onClick={() => submit()}> 업로드 하기</button>
-    </div>
+    <Control>
+        <ImagePreview
+          image={image}
+          onClick={() => {
+            setShowImage(!showImage);
+          }}
+        />
+        <TakePhotoButton
+          onClick={() => {
+            if (camera.current) {
+              const photo = camera.current.takePhoto();
+              console.log(photo);
+              setImage(photo);
+            }
+          }}
+        />
+        <ChangeFacingCameraButton
+          disabled={numberOfCameras <= 1}
+          onClick={() => {
+            if (camera.current) {
+              const result = camera.current.switchCamera();
+              console.log(result);
+            }
+          }}
+        />
+      </Control>
+    </Wrapper>
   );
 }
 
